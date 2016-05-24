22bd857da440b0b8b57bc25a34251e5bca3e1083
ralinux <ralinux@localhost.localdomain>
2016-05-24 13:55:10 +0000

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name centroEureApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the centroEureApp
+ */
+angular.module('centroEureApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name centroEureApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the centroEureApp
+ */
+angular.module('centroEureApp')
+  .controller('MainCtrl', function () {
+  	var vm = this;
+  });
