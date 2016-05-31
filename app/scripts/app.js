ad1379b429fc06bdcbc7bb6c27ed4eae9a921e97
Rafastaria <guitronleafar@gmail.com>
2016-05-31 11:21:59 +0000

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name centroEureApp
+ * @description
+ * # centroEureApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('centroEureApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngMaterial'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'vm'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'vm'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name centroEureApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the centroEureApp
+ */
+angular.module('centroEureApp')
+  .controller('MainCtrl', function () {
+  	var vm = this;
+  });
+
+
+  
+
+  
